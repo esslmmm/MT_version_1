a9bd23a697f20492a0f5c4917d24f1911de83700
esslmmm <http.esnonofficial@gmail.com>
2025-04-20 10:25:35 +0700

    fix HomePageProps

--- mt-v1.0.0/app/page.tsx
+++ mt-v1.0.0/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Advertisement from "@/components/user/hompage/Advertisement";
 import "./globals.css";
@@ -6,13 +6,13 @@ import MedicalList from "@/components/user/hompage/MedicalList";
 import Footer from "@/components/user/main/Footer";
 import Hospitaltap from "@/components/user/hompage/Hospitaltap";
 
-const HomePage = ({ children }: { children: React.ReactNode }) => {
+const HomePage = () => {
   return (
     <div>
       <Advertisement />
       <div className="text-center pt-10 pb-8">
         <h1 className="font-semibold text-[#4D4D4D]" style={{ fontSize: 35 }}>
-          Select your package <br /> 
+          Select your package <br />
         </h1>
         <b className="text-[#717171] font-regular" style={{ fontSize: 16 }}>
           Choose the package that's right for you?
@@ -21,7 +21,6 @@ const HomePage = ({ children }: { children: React.ReactNode }) => {
       <MedicalList />
       <Hospitaltap />
       <Footer />
-      {children}
     </div>
   );
 };
