c91228da148062f4767beb209e5cf2631e81782f
SONDETH-bpn <6531501229@lamduan.mfu.ac.th>
2025-04-21 16:07:07 +0700

    Change

--- mt-v1.0.0/components/user/package/OfferService.tsx
+++ mt-v1.0.0/components/user/package/OfferService.tsx
@@ -36,23 +36,21 @@ export default function OfferService() {
       {/* Book Button Styled as a Div */}
         <div
           onClick={navigateToMedicalAppointment}
-          className="cursor-pointer w-2/3 sm:w-1/2 md:w-2/3 bg-green-400 text-green-600 text-lg px-8 py-4 font-semibold rounded-2xl hover:bg-green-500 hover:text-white transition duration-300 flex justify-center mx-auto"
+          className="cursor-pointer w-2/3 sm:w-1/2 md:w-2/3 bg-green-400 text-white text-lg px-8 py-4 font-semibold rounded-2xl hover:bg-green-700 transition duration-300 flex justify-center mx-auto"
         >
           Book
         </div>
 
-
-
       {/* Services Section */}
       <section className="py-12 text-center">
-        <h2 className="text-2xl font-bold mb-6">Offer Service</h2>
+        <h2 className="text-2xl text-[#000000] font-bold mb-6">Offer Service</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
           {services.map((service, index) => (
             <div
               key={index}
               className="bg-white shadow-md rounded-xl p-6 border border-gray-200 hover:shadow-lg transition"
             >
-              <h3 className="font-semibold text-lg">{service.title}</h3>
+              <h3 className="font-semibold text-[#000000] text-lg">{service.title}</h3>
               {service.description && (
                 <p className="text-gray-600 text-sm mt-2">{service.description}</p>
               )}
