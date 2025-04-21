35983fac1647095f163cf0fea912e49c533c49d0
SONDETH-bpn <6531501229@lamduan.mfu.ac.th>
2025-04-21 16:23:24 +0700

    ...

--- mt-v1.0.0/components/user/package/HospitalCard.tsx
+++ mt-v1.0.0/components/user/package/HospitalCard.tsx
@@ -35,11 +35,12 @@ const HospitalCard = () => {
 
           {/* Hospital Details */}
           <div className="text-center sm:text-left flex-1">
-            <h2 className="text-lg font-semibold">{hospital.name}</h2>
-            <p className="flex justify-center sm:justify-start items-center text-gray-600 text-sm mt-1">
-              <FaMapMarkerAlt className="text-red-500 mr-1" />
-              {hospital.address}
+            <h2 className="text-lg text-[#000000] font-semibold">{hospital.name}</h2>
+            <p className="flex flex-col sm:flex-row justify-center sm:justify-start items-center text-gray-600 text-sm mt-1 text-center sm:text-left break-words">
+              <FaMapMarkerAlt className="text-red-500 mr-1 mb-1 sm:mb-0" />
+              <span>{hospital.address}</span>
             </p>
+
 
             {/* Button */}
             <button
